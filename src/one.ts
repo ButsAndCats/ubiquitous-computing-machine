7710be1f847311383f0a34dbc4d56dff035194fe
George <me@georgebutter.com>
2021-05-12 21:59:09 +1000

    feat: add alternative unique function

--- src/one.ts
+++ src/one.ts
@@ -1,7 +1,13 @@
 /**
  * A function that takes an array of strings and returns 
- * the an array only containing unique values in the array.
+ * an array only containing unique values in the array.
  */
+
+// This version removes any duplicates.
 const uniqueArrayItems = (arr: Array<string>): Array<string> => arr.filter((value, index, self) => self.indexOf(value) === index);
 
+// This version removes non unique items entirely.
+const removedUniqueArrayItems = (arr: Array<string>): Array<string> => arr.filter((value, index, self) => self.lastIndexOf(value) === self.indexOf(value));
+
 console.log(uniqueArrayItems(["test", "test", "foo", "bar", "foo"]))
+console.log(removedUniqueArrayItems(["test", "test", "test", "bar", "foo"]))
